890a8c6e869f6afe55c9f5493f2d0cf52bffafdd
Deekay yadav <deekay843424@gmail.com>
2026-01-31 08:48:13 +0530

    00

--- resources/views/supplier.blade.php
+++ resources/views/supplier.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 @section('title', 'Supplier')
 @section('section')
-    <section class="page-header py-5 bg-primary">
+    {{-- <section class="page-header py-5 bg-primary">
         <div class="container">
             <h1 class="text-center text-white">Supplier</h1>
         </div>
-    </section>
+    </section> --}}
     @include('components.pricing')
      @include('components.testimonial')
     @include('components.cta-section')
