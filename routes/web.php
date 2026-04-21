4f0fd5dd471bd8459b0e055aa273f8e5654f0142
Deekay yadav <deekay843424@gmail.com>
2026-04-21 12:53:23 +0530

    fds

--- routes/web.php
+++ routes/web.php
@@ -113,7 +113,7 @@ Route::middleware(['auth', 'verified', 'role:supplier'])->prefix('supplier-panel
 
 Route::middleware(['auth', 'verified', 'role:customer'])->prefix('customer-panel')->name('customer-panel.')->group(function () {
     Route::get('/dashboard', [CustomerPanelController::class, 'dashboard'])->name('dashboard');
-    // Route::get('/suppliers', [CustomerPanelController::class, 'suppliers'])->name('suppliers');
+    Route::get('/suppliers', [CustomerPanelController::class, 'suppliers'])->name('suppliers');
     Route::get('/jobs', [CustomerPanelController::class, 'jobs'])->name('jobs');
     Route::post('/jobs', [CustomerPanelController::class, 'store'])->name('jobs.store');
     Route::get('/edit-job/{job}', [CustomerPanelController::class, 'editJob'])->name('jobs.edit');
